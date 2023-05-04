d243cbc1f778226655b1ac09f2ad23508bae1d4b
Calvin Zhang 1993 <121672580+czhang1993@users.noreply.github.com>
2023-05-04 20:32:00 +1000

    Update test_svi.py

--- tests/test_svi.py
+++ tests/test_svi.py
@@ -1,15 +1,19 @@
-import VI, elbo
-from tests.data import x_train, x_test, y_train, y_test
+import torch
+from torch.optim import Adam
+from ..svi import VI, elbo
+from .data import x_train, x_test, y_train, y_test
+
+x_train = torch.Tensor(x_train)
+y_train = torch.Tensor(y_train)
 
 epochs = 500
 
 model = VI()
-optim = torch.optim.Adam(model.parameters())
+optim = Adam(model.parameters())
 
 for epoch in range(epochs):
     optim.zero_grad()
-    y_pred, mu, log_var = model(x)  # need to change
-    loss = -elbo(y, y_pred, mu, log_var)  # need to change
+    y_pred, mu, log_var = model(x_train)  # need to change
+    loss = -elbo(y_train, y_pred, mu, log_var)  # need to change
     loss.backward()
     optim.step()
-    
