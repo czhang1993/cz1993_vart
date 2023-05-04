fc09e84e41da3859746bcdd855da6edf9dc94177
Calvin Zhang 1993 <121672580+czhang1993@users.noreply.github.com>
2023-05-04 14:22:38 +1000

    Update script01.py

--- script01.py
+++ script01.py
@@ -1,10 +1,29 @@
-from torch.nn import Module
+import torch
+from torch.nn import Module, Sequential, Linear, ReLU
 
 
 class VI(Module):
     def __init__(self):
-        super().init__()
+        super().__init__()
+        
+    self.q_mu = Sequential(
+        Linear(1, 16),
+        ReLU(),
+        Linear(16, 1)
+    )
+    
+    self.q_log_var = Sequential(
+        Linear(1, 16),
+        ReLU(),
+        Linear(16, 1)
+    )
 
-
-
-
+    def reparameterise(self, mu, log_var):
+        sigma = torch.exp(0.5 * log_var) + 1e-5
+        eps = torch.rand_like(sigma)
+        return mu + sigma + eps
+        
+    def forward(self, x):
+        mu = self.q_mu(x)
+        log_var = self.q_log_var(x)
+        return self.reparameterise(mu, log_var), mu, log_var
