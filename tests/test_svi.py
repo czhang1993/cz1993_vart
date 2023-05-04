922e59b58a1fccc5c4e012b0bad9ba45a65af21c
Calvin Zhang 1993 <121672580+czhang1993@users.noreply.github.com>
2023-05-04 14:46:52 +1000

    Update and rename test_svi to test_svi.py

--- tests/test_svi.py
+++ tests/test_svi.py
@@ -1,4 +1,5 @@
 from cz1993_vart import VI, elbo
+from cz1993_vart.tests import x_train, x_test, y_train, y_test
 
 epochs = 500
 
@@ -7,8 +8,8 @@ optim = torch.optim.Adam(model.parameters())
 
 for epoch in range(epochs):
     optim.zero_grad()
-    y_pred, mu, log_var = model(x)
-    loss = -elbo(y, y_pred, mu, log_var)
+    y_pred, mu, log_var = model(x)  # need to change
+    loss = -elbo(y, y_pred, mu, log_var)  # need to change
     loss.backward()
     optim.step()
     
