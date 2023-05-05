81639bbfdf6a8e5be3129afb18ede1781a6a9609
Calvin Zhang 1993 <121672580+czhang1993@users.noreply.github.com>
2023-05-05 19:04:17 +1000

    Update random_forest.py

--- algorithms/random_forest.py
+++ algorithms/random_forest.py
@@ -9,6 +9,7 @@ def entropy(p):
     else:
         return -(p * np.log2(p) + (1 - p) * np.log2(1 - p))
 
+
 def info_gain(left_child, right_child):
     parent = left_child + right_child
     p_parent = parent.count(1) / len(parent) if len(parent) > 0 else 0
@@ -26,9 +27,9 @@ def info_gain(left_child, right_child):
 def draw_boot(x_train, y_train):
     boot_indices = list(
         np.random.choice(
-            range(len(x_train)), 
-            len(x_train), 
-            replace = True
+            range(len(x_train)),
+            len(x_train),
+            replace=True
         )
     )
     oob_indices = [i for i in range(len(x_train)) if i not in boot_indices]
@@ -49,79 +50,79 @@ def oob_score(tree, x_test, y_test):
 
 
 def find_split_point(
-    x_boot,
-    y_boot,
-    max_features
+        x_boot,
+        y_boot,
+        max_features
 ):
     feature_ls = list()
     num_features = len(x_boot[0])
 
     while len(feature_ls) <= max_features:
-    feature_idx = random.sample(range(num_features), 1)
-    if feature_idx not in feature_ls:
-        feature_ls.extend(feature_idx)
+        feature_idx = np.random.sample(range(num_features), 1)
+        if feature_idx not in feature_ls:
+            feature_ls.extend(feature_idx)
 
     best_info_gain = -999
     node = None
     for feature_idx in feature_ls:
-    for split_point in x_boot[:,feature_idx]:
-        left_child = {"x_boot": [], "y_boot": []}
-        right_child = {"x_boot": [], "y_boot": []}
+        for split_point in x_boot[:, feature_idx]:
+            left_child = {"x_boot": [], "y_boot": []}
+            right_child = {"x_boot": [], "y_boot": []}
 
-        # split children for continuous variables
-        if type(split_point) in [int, float]:
-            for i, value in enumerate(x_boot[:,feature_idx]):
-                if value <= split_point:
-                    left_child["x_boot"].append(x_boot[i])
-                    left_child["y_boot"].append(y_boot[i])
-                else:
-                    right_child["x_boot"].append(x_boot[i])
-                    right_child["y_boot"].append(y_boot[i])
-        # split children for categoric variables
-        else:
-            for i, value in enumerate(x_boot[:, feature_idx]):
-                if value == split_point:
-                    left_child["x_boot"].append(x_boot[i])
-                    left_child["y_boot"].append(y_boot[i])
-                else:
-                    right_child["x_boot"].append(x_boot[i])
-                    right_child["y_boot"].append(y_boot[i])
+            # split children for continuous variables
+            if type(split_point) in [int, float]:
+                for i, value in enumerate(x_boot[:, feature_idx]):
+                    if value <= split_point:
+                        left_child["x_boot"].append(x_boot[i])
+                        left_child["y_boot"].append(y_boot[i])
+                    else:
+                        right_child["x_boot"].append(x_boot[i])
+                        right_child["y_boot"].append(y_boot[i])
+            # split children for categorical variables
+            else:
+                for i, value in enumerate(x_boot[:, feature_idx]):
+                    if value == split_point:
+                        left_child["x_boot"].append(x_boot[i])
+                        left_child["y_boot"].append(y_boot[i])
+                    else:
+                        right_child["x_boot"].append(x_boot[i])
+                        right_child["y_boot"].append(y_boot[i])
 
-        split_info_gain = info_gain(
-            left_child["y_boot"], 
-            right_child["y_boot"]
-        )
-        if split_info_gain > best_info_gain:
-            best_info_gain = split_info_gain
-            left_child["x_boot"] = np.array(left_child["x_boot"])
-            right_child["x_boot"] = np.array(right_child["x_boot"])
-            node = {
-                "info_gain": split_info_gain,
-                "left_child": left_child,
-                "right_child": right_child,
-                "split_point": split_point,
-                "feature_idx": feature_idx
-            }
+            split_info_gain = info_gain(
+                left_child["y_boot"],
+                right_child["y_boot"]
+            )
+            if split_info_gain > best_info_gain:
+                best_info_gain = split_info_gain
+                left_child["x_boot"] = np.array(left_child["x_boot"])
+                right_child["x_boot"] = np.array(right_child["x_boot"])
+                node = {
+                    "info_gain": split_info_gain,
+                    "left_child": left_child,
+                    "right_child": right_child,
+                    "split_point": split_point,
+                    "feature_idx": feature_idx
+                }
     return node
 
 
 def terminal_node(node):
     y_boot = node["y_boot"]
-    pred = max(y_boot, key = y_boot.count)
+    pred = max(y_boot, key=y_boot.count)
     return pred
 
 
 def split_node(
-    node,
-    max_features,
-    min_samples_split,
-    max_depth, depth
+        node,
+        max_features,
+        min_samples_split,
+        max_depth, depth
 ):
     left_child = node["left_child"]
     right_child = node["right_child"]
 
-    del(node["left_child"])
-    del(node["right_child"])
+    del (node["left_child"])
+    del (node["right_child"])
 
     if len(left_child["y_boot"]) == 0 or len(right_child["y_boot"]) == 0:
         empty_child = {
@@ -166,14 +167,14 @@ def split_node(
             max_depth,
             depth + 1
         )
-        
-        
+
+
 def build_tree(
-    x_boot,
-    y_boot,
-    max_depth,
-    min_samples_split,
-    max_features
+        x_boot,
+        y_boot,
+        max_depth,
+        min_samples_split,
+        max_features
 ):
     root_node = find_split_point(
         x_boot,
@@ -191,12 +192,12 @@ def build_tree(
 
 
 def random_forest(
-    x_train,
-    y_train,
-    n_estimators,
-    max_features,
-    max_depth,
-    min_samples_split
+        x_train,
+        y_train,
+        n_estimators,
+        max_features,
+        max_depth,
+        min_samples_split
 ):
     tree_ls = list()
     oob_ls = list()
@@ -236,4 +237,3 @@ def predict_tree(tree, x_test):
             )
         else:
             return tree["right_split"]
-        
