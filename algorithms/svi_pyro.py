48d49cb4e449919ff878e9ffe1bc2befbdb265e5
Calvin Zhang 1993 <121672580+czhang1993@users.noreply.github.com>
2023-05-09 15:40:14 +1000

    Update svi_pyro.py

--- algorithms/svi_pyro.py
+++ algorithms/svi_pyro.py
@@ -1,9 +1,18 @@
+from torch import Tensor
+from pyro import sample
 from pyro.distributions.dist import Beta, Bernoulli
 
 
 def model(data):
-    alpha0 = torch.tensor(10.0)
-    beta0 = torch.tensor(10.0)
-    f = pyro.sample("latent_fairness", dist.Beta(alpha0, beta0))
+    alpha0 = Tensor(10.0)
+    beta0 = Tensor(10.0)
+    f = sample(
+        "latent_fairness",
+        Beta(alpha0, beta0)
+    )
     for i in range(len(data)):
-        pyro.sample("obs_{}".format(i), dist.Bernoulli(f), obs=data[i])
+        sample(
+            "obs_{}".format(i),
+            Bernoulli(f),
+            obs=data[i]
+        )
