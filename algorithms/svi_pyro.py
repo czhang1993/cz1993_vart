c7ef6b5cba5365bcfdda0d7321edb22ca7b566b7
Calvin Zhang 1993 <121672580+czhang1993@users.noreply.github.com>
2023-05-09 11:20:40 +1000

    Update svi_pyro.py

--- algorithms/svi_pyro.py
+++ algorithms/svi_pyro.py
@@ -1,1 +1,14 @@
+from torch import Tensor
 
+
+def model(data):
+    pass
+  
+  
+def guide(data):
+    # parameter mu squared of the q distribution. 
+    q_mu = pyro.param("q_mu", Tensor(?))
+    # parameter sigma squared of the q distribution. 
+    # the constraint is from torch.distributions.constraints.Constraint
+    q_sigsq = pyro.param("q_sigsq", Tensor(?), constraint=constraints.positive)
+    
